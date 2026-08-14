a5ca8740f38b8a49b0f60616ac7ad9df2805d75f
nitya-tomar <nityasingh284@gmail.com>
2026-08-15 00:46:05 +0530

    NEW

--- Day3.py
+++ Day3.py
@@ -9,3 +9,8 @@ server = {"name": "web-01", "cpu_usage": 85, "region": "us-east-1"}
 
 for key, value in server.items():
     print(key, ":", value)
+
+# 1. Create a dictionary for a laptop with keys brand, ram, price. Print each key and value using a loop.
+dict_laptop= {"Brand" :"HP", "RAM":"16GB", "Price":"$1200"}
+for key, value in dict_laptop.items():
+    print(key, ":", value)
