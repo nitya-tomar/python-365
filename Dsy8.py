94b68fa966c89a7cdfa60a4213ded214f1bfa9d4
nitya-tomar <nityasingh284@gmail.com>
2026-08-19 23:08:45 +0530

    listnew

---
+++ Dsy8.py
@@ -0,0 +1,6 @@
+#Given numbers = [10, 20, 30, 40, 50, 60], print only the last 3 items using slicing.
+
+numbers = [10, 20, 30, 40, 50, 60]
+# Using slicing to get the last 3 items
+newlist = numbers[-3:]
+print(newlist)  
