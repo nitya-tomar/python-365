09878d7eec58f88e0be3f7fe5b0fd26fbe7b7ae9
nitya-tomar <nityasingh284@gmail.com>
2026-08-19 23:13:19 +0530

    new

--- Dsy8.py
+++ Dsy8.py
@@ -4,3 +4,12 @@ numbers = [10, 20, 30, 40, 50, 60]
 # Using slicing to get the last 3 items
 newlist = numbers[-3:]
 print(newlist)  
+
+
+#Given cpu_usage = [45, 92, 78, 60, 88], use a loop to count how many values are above 80.
+cpu_usage = [45, 92, 78, 60, 88]
+count=0
+for usage in cpu_usage: 
+    if usage > 80:
+        count += 1
+print(count)               
