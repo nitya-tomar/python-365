89443ae4b5103155e0e8ea1087f8f133ba90b9a4
nitya-tomar <nityasingh284@gmail.com>
2026-08-22 22:55:57 +0530

    enumerate

---
+++ day11.py
@@ -0,0 +1,6 @@
+fruits = ["apple", "banana", "cherry"]  # a simple list of items
+
+for index, fruit in enumerate(fruits):
+    # enumerate() gives two things at once: the position (index) and the value (fruit)
+    
+    print(index, fruit)   
